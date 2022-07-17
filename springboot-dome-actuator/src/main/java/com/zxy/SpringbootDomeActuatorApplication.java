6b6d6d1f6c8d329f2f1c2a0815621539f4363690
ZXYAISWH <3260653092@qq.com>
2022-07-17 16:21:30 +0800

    springboot-dome-actuator springboot的监控

---
+++ springboot-dome-actuator/src/main/java/com/zxy/SpringbootDomeActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.zxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDomeActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDomeActuatorApplication.class, args);
+    }
+
+}
